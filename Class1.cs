aca7a818455d0a724385e2edc0e7f3464299f624
alexr03 <alexredding180@gmail.com>
2016-10-08 10:50:03 +0100

    SHIT

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketPluginTemplate
+{
+    public class Main : RocketPlugin
+    {
+        protected override void Load()
+        {
+
+        }
+
+        protected override void Unload()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+        }
+    }
+}
