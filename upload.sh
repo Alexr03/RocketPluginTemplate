5a189c3cf01c8829940be8084da980d9a8078604
Alex <alexredding180@gmail.com>
2016-10-28 23:17:20 +0100

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+HOST='ftp.solarsentinels.co.uk/public_html/'
+USER='solar'
+PASSWD='Uv#-j!bbQ"f_Qv66'
+FILE='bin/debug/RocketPluginTemplate.dll'
+
+ftp -n $HOST <<END_SCRIPT
+quote USER $USER
+quote PASS $PASSWD
+put $FILE
+quit
+END_SCRIPT
+exit 0
